babcec25d5b088afe9d1e85ac00b3ee47387b9d7
numberwang55 <cban0901@hotmail.com>
2024-04-24 15:48:05 +0100

    fetchUser pause async function to test rendering

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,7 +3,16 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
+  // Dev only
+  await pause(1000)
   return response.data;
 });
 
 export { fetchUsers };
+
+// DEV only
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
