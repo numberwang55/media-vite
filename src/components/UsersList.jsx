4cc3c2b27c299e713a483e8d69832af590b06101
numberwang55 <cban0901@hotmail.com>
2024-04-24 15:31:13 +0100

    Update: Userslist retrieve data from slice

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,3 +1,24 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+
 export default function UsersList() {
-  return "Users List";
+  const dispatch = useDispatch()
+  const {data, isLoading, error} = useSelector(state => {
+    return state.users
+  })
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>
+  }
+
+  return <div>{data.length}</div>;
 }
