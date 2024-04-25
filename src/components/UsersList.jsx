4bbb25b3788183303f901908340d63c98bdd1bd7
numberwang55 <cban0901@hotmail.com>
2024-04-25 11:57:08 +0100

    Update: UsersList - add user button

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,33 +1,48 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { fetchUsers, addUser } from "../store";
+import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 export default function UsersList() {
-  const dispatch = useDispatch()
-  const {data, isLoading, error} = useSelector(state => {
-    return state.users
-  })
+  const dispatch = useDispatch();
+  const { data, isLoading, error } = useSelector((state) => {
+    return state.users;
+  });
 
   useEffect(() => {
-    dispatch(fetchUsers())
-  }, [])
+    dispatch(fetchUsers());
+  }, []);
+
+  const handleUserAdd = () => {
+    dispatch(addUser())
+  }
 
   if (isLoading) {
-    return <Skeleton times={6} className="h-10 w-full"/>
+    return <Skeleton times={6} className="h-10 w-full" />;
   }
 
   if (error) {
-    return <div>Error fetching data...</div>
+    return <div>Error fetching data...</div>;
   }
 
-  const renderedUsers = data.map(user => {
-    return <div key={user.id} className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        {user.name}
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
       </div>
-    </div>
-  })
+    );
+  });
 
-  return <div>{renderedUsers}</div>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
