44e24dd6fc4729bf3d23ca4ffe7dcdb39c07a3cb
numberwang55 <cban0901@hotmail.com>
2024-04-25 11:08:47 +0100

    Feature: Skeleton loading component

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
   // Dev only
-  await pause(1000)
+  await pause(10000)
   return response.data;
 });
 
