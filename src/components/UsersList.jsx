e3e355143cd033f5c9dac5f63c49ab49e771dc0f
numberwang55 <cban0901@hotmail.com>
2024-04-26 09:04:03 +0100

    Refactor: UsersList - render skeleton/error below top bar

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -6,49 +6,48 @@ import Skeleton from "./Skeleton";
 import useThunk from "../hooks/use-thunk";
 
 export default function UsersList() {
-  const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers)
-  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser)
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
 
   const { data, isLoading, error } = useSelector((state) => {
     return state.users;
   });
 
   useEffect(() => {
-    doFetchUsers()
+    doFetchUsers();
   }, [doFetchUsers]);
 
   const handleUserAdd = () => {
-    doCreateUser()
+    doCreateUser();
   };
 
+  let content;
   if (isLoadingUsers) {
-    return <Skeleton times={6} className="h-10 w-full" />;
-  }
-
-  if (loadingUsersError) {
-    return <div>Error fetching data...</div>;
-  }
-
-  const renderedUsers = data.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map((user) => {
+      return (
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">
+            {user.name}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  }
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUser ? ("Creating User...") : (
-          <Button onClick={handleUserAdd}>+ Add User</Button>
-        )}
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
         {creatingUserError && "Error creating user..."}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
