d1ff8fba5b14c1723a0b3e7d4c00162852f6214a
numberwang55 <cban0901@hotmail.com>
2024-04-26 10:45:01 +0100

    Refactor: UsersList - created UsersListItem

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,10 @@ import { usersReducer } from "./slices/usersSlice";
 
 export const store = configureStore({
   reducer: {
-    users: usersReducer
-  }
-})
+    users: usersReducer,
+  },
+});
 
-export * from "./thunks/fetchUsers"
-export * from "./thunks/addUser"
+export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser";
+export * from "./thunks/deleteUser";
