165dd8225a7ebfa4a06d716155386ef50d1a8a14
numberwang55 <cban0901@hotmail.com>
2024-04-29 15:46:44 +0100

    Feature: albumsApi - Redux Toolkit Query

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,4 +1,9 @@
+import { useFetchAlbumsQuery } from "../store"
+
 export default function AlbumsList({user}) {
+  const {data, error, isLoading} = useFetchAlbumsQuery(user)
+  console.log(error, isLoading, data);
+
   return (
     <div>
       Albums for {user.name}
