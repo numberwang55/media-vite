0dd0bdad0c9797f5b2897a387a109055d359b882
numberwang55 <cban0901@hotmail.com>
2024-04-29 08:54:13 +0100

    Update: ExpandablePanel - component state, conditional rendering based on expanded state

--- src/components/ExpandablePanel.jsx
+++ src/components/ExpandablePanel.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 export default function ExpandablePanel({ children, header }) {
+  const [expanded, setExpanded] = useState(false)
+  const handleClick = () => {
+    setExpanded(!expanded)
+  }
+
   return (
     <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="flex p-2 justify-between items-center">
         <div className="flex flex-row items-center justify-between">{header}</div>
+        <div onClick={handleClick} className="cursor-pointer text-xl">
+        {expanded ? <GoChevronDown/> : <GoChevronLeft/>}
+        </div>
       </div>
-      <div className="p-2 border-t">{children}</div>
+      { expanded && <div className="p-2 border-t">{children}</div>}
     </div>
   );
 }
