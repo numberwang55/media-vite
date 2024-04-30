7eda59397901efc1b0b78343f2bf602f3da08580
numberwang55 <cban0901@hotmail.com>
2024-04-30 20:56:46 +0100

    Update: albumsApi tags change

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 import AlbumsListItem from "./AlbumsListItem";
 
 export default function AlbumsList({ user }) {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
 
   const handleAddAlbum = () => {
@@ -13,7 +13,7 @@ export default function AlbumsList({ user }) {
   };
 
   let content;
-  if (isLoading) {
+  if (isFetching) {
     content = <Skeleton times={3} className="h-10 w-full"/>;
   } else if (error) {
     content = <div>Error Loading Albums</div>;
