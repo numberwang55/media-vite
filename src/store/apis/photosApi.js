3aaf1c7b7ae76878b35e4c38fea01ed5c8701ae5
numberwang55 <cban0901@hotmail.com>
2024-05-01 10:14:41 +0100

    Feature: photosApi created and connected to store

---
+++ src/store/apis/photosApi.js
@@ -0,0 +1,52 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
+
+const photosApi = createApi({
+  reducerPath: "photos",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      fetchPhotos: builder.query({
+        query: (album) => {
+          return {
+            url: "/photos",
+            // default is GET, could be omitted
+            method: "GET",
+            params: {
+              albumId: album.id
+            }
+          }
+        }
+      }),
+      addPhoto: builder.mutation({
+        query: (album) => {
+          return {
+            url: "/photos",
+            method: "POST",
+            body: {
+              albumId: album.id,
+              url: faker.image.urlLoremFlickr({ category: 'abstract', height: 150, width: 150 })
+            }
+          }
+        }
+      }),
+      deletePhoto: builder.mutation({
+        query: (photo) => {
+          return {
+            url: `/photos${photo.id}`,
+            method: "DELETE"
+          }
+        }
+      })
+    }
+  }
+})
+
+export const {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useDeletePhotoMutation
+} = photosApi
+export { photosApi }
